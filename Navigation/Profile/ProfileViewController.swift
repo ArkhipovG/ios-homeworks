37255750a211594b1df173c7f6fc59a0c531ed10
ArkhipovG <116101092+ArkhipovG@users.noreply.github.com>
2023-04-04 10:09:45 +0300

    CollectionView HW

--- Navigation/Profile/ProfileViewController.swift
+++ Navigation/Profile/ProfileViewController.swift
@@ -11,25 +11,23 @@ class ProfileViewController: UIViewController {
 
     private enum Constants {
         static let reuseIdentifire = "cellID"
+        static let collectionID = "collectID"
     }
 
-    let publicationsArray = PostProvider.getPost()
+    private let publicationsArray = PostProvider.getPost()
     private let headerView = ProfileHeaderView()
-
+    private var arrayOfImages: [UIImage] = ImageProvider.getImages()
 
     private lazy var tableView: UITableView = {
 
         let tableView = UITableView(frame: .zero, style: .grouped)
-        self.tableView = UITableView(frame: view.bounds, style: .grouped)
         tableView.register(PostTableViewCell.self, forCellReuseIdentifier: Constants.reuseIdentifire )
         tableView.dataSource = self
         tableView.delegate = self
         tableView.separatorStyle = .none
         tableView.translatesAutoresizingMaskIntoConstraints = false
-
         return tableView
     }()
-
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -37,15 +35,17 @@ class ProfileViewController: UIViewController {
         view.addSubview(tableView)
         self.tableView.register(PostTableViewCell.self, forCellReuseIdentifier: Constants.reuseIdentifire)
         addConstraintsOfTableView()
+        self.tableView.register(PhotosTableViewCell.self, forCellReuseIdentifier: Constants.collectionID)
+        tableView.sectionFooterHeight = 0.0
+
     }
 
-    func addConstraintsOfTableView() {
+    private func addConstraintsOfTableView() {
         NSLayoutConstraint.activate([
             tableView.topAnchor.constraint(equalTo: view.topAnchor),
             tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
             tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
-
         ])
     }
 }
@@ -53,18 +53,43 @@ class ProfileViewController: UIViewController {
 // UIDataSource
 extension ProfileViewController: UITableViewDataSource, UITableViewDelegate {
 
+
+    func numberOfSections(in tableView: UITableView) -> Int {
+        2
+    }
+
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        publicationsArray.count
+        if section == 0 {
+            return 1
+        } else {
+            return publicationsArray.count
+        }
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: Constants.reuseIdentifire, for: indexPath) as! PostTableViewCell
-        let post = publicationsArray[indexPath.row]
-        cell.configure(post: post)
-        return cell
+        if indexPath.section == 0 {
+            let cell = tableView.dequeueReusableCell(withIdentifier: Constants.collectionID, for: indexPath) as! PhotosTableViewCell
+            cell.configure(imagesArray: Array(arrayOfImages.prefix(4)))
+            return cell
+        } else {
+            let cell = tableView.dequeueReusableCell(withIdentifier: Constants.reuseIdentifire, for: indexPath) as! PostTableViewCell
+            let post = publicationsArray[indexPath.row]
+            cell.configure(post: post)
+            return cell
+        }
     }
 
     func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
-        return headerView
+        if section == 0 {
+            return headerView
+        } else {
+            return .none
+        }
     }
+
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let newViewController = PhotosViewController(array: arrayOfImages)
+        navigationController?.pushViewController(newViewController, animated: true)
+    }
+
 }
